ae99efd14c382e5a9880e9a1528b0fdce26c501e
AnjasSM <syarifudin.anjas@gmail.com>
2018-11-21 12:21:38 +0700

    ini commit2

--- src/components/HomeBertasbih.js
+++ src/components/HomeBertasbih.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class HomeBertasbih extends Component {
     render() {
         return (
             <div>
                 <h1>Ini Home</h1>
+                <h2>{this.props.pikachu}</h2>
             </div>
         )
     }
+};
+
+const mapStateToProps = (state) => {
+    return { pikachu: state.pikachu };
 }
 
-export default HomeBertasbih;
+export default connect(mapStateToProps)(HomeBertasbih);
